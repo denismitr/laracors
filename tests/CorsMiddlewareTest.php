7d3a37fb70bb7ef01c3e1cdcfedaf0ce6654a231
Denis Mitrofanov <denis.mitr@gmail.com>
2017-06-10 18:38:26 +0300

    Adds more tests

--- tests/CorsMiddlewareTest.php
+++ tests/CorsMiddlewareTest.php
@@ -8,6 +8,29 @@ use Illuminate\Http\Response;
 
 class CorsMiddlewareTest extends \Orchestra\Testbench\TestCase
 {
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('laracors', [
+            'allowed_origins' => [
+                "/.+/"
+            ],
+
+            'all_allowed_methods' => [
+                'HEAD', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'
+            ],
+
+            'allowed_headers' => [
+                'Content-Type', 'Accept', 'Authorization', 'Location', "Origin", 'Requested'
+            ]
+        ]);
+}
 
     /** @test */
     public function it_checks_if_correct_headers_are_set()
@@ -58,6 +81,90 @@ class CorsMiddlewareTest extends \Orchestra\Testbench\TestCase
     }
 
     /** @test */
+    public function it_can_change_allowed_headers_through_config()
+    {
+        $this->app['config']->set('laracors.allowed_headers', [
+            'Content-Type', 'Authorization'
+        ]);
+
+        $request = $this->mockRequest("OPTIONS", 'http://example.com');
+        $response = new Response;
+        $cors = new Cors;
+
+        $response = $cors->handle($request, function() use ($response) {
+            return $response;
+        });
+
+        $this->assertEquals($response->headers->get('Access-Control-Allow-Origin'), 'http://example.com');
+
+        $this->assertEquals(
+            $response->headers->get('Access-Control-Allow-Methods'),
+            'HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS'
+        );
+
+        $this->assertEquals(
+            $response->headers->get('Access-Control-Allow-Headers'),
+            'Content-Type, Authorization'
+        );
+    }
+
+    /** @test */
+    public function it_can_change_allowed_origins_through_config()
+    {
+        $this->app['config']->set('laracors.allowed_origins', [
+            "/http(s)?:\/\/(www\.)?localhost(:[0-9]+)?/"
+        ]);
+
+        $request = $this->mockRequest("OPTIONS", 'http://localhost:8000');
+        $response = new Response;
+        $cors = new Cors;
+
+        $response = $cors->handle($request, function() use ($response) {
+            return $response;
+        });
+
+        $this->assertEquals($response->headers->get('Access-Control-Allow-Origin'), 'http://localhost:8000');
+
+        $this->assertEquals(
+            $response->headers->get('Access-Control-Allow-Methods'),
+            'HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS'
+        );
+
+        $this->assertEquals(
+            $response->headers->get('Access-Control-Allow-Headers'),
+            'Content-Type, Accept, Authorization, Location, Origin, Requested'
+        );
+    }
+
+    /** @test */
+    public function it_can_change_allowed_origins_through_config_and_prohibt_headers_for_non_match()
+    {
+        $this->app['config']->set('laracors.allowed_origins', [
+            "/http(s)?:\/\/(www\.)?localhost(:[0-9]+)?/"
+        ]);
+
+        $request = $this->mockRequest("OPTIONS", 'http://example.com');
+        $response = new Response;
+        $cors = new Cors;
+
+        $response = $cors->handle($request, function() use ($response) {
+            return $response;
+        });
+
+        $this->assertEquals($response->headers->get('Access-Control-Allow-Origin'), null);
+
+        $this->assertEquals(
+            $response->headers->get('Access-Control-Allow-Methods'),
+            null
+        );
+
+        $this->assertEquals(
+            $response->headers->get('Access-Control-Allow-Headers'),
+            null
+        );
+    }
+
+    /** @test */
     public function cors_can_be_told_to_accept_certail_request_methods_only()
     {
         $request = $this->mockRequest("POST", 'http://example.com');
@@ -94,13 +201,13 @@ class CorsMiddlewareTest extends \Orchestra\Testbench\TestCase
 
         $response = $cors->handle($request, function() use ($response) {
             return $response;
-        }, 'post,put');
+        }, 'get,post,put');
 
         $this->assertEquals($response->headers->get('Access-Control-Allow-Origin'), 'http://example.com');
 
         $this->assertEquals(
             $response->headers->get('Access-Control-Allow-Methods'),
-            'POST, PUT'
+            'GET, POST, PUT'
         );
 
         $this->assertEquals(
